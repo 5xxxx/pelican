1ba1d5f75ec02ab62f9aa9119772cf18a64982e4
lintao <mrqter@gmail.com>
2021-08-19 16:08:33 +0800

    add resp

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/go-playground/locales v0.14.0
 	github.com/go-playground/universal-translator v0.18.0
 	github.com/go-playground/validator/v10 v10.9.0
+	github.com/labstack/echo/v4 v4.5.0
 	go.mongodb.org/mongo-driver v1.7.1
 )
---
+++ resp/resp.go
@@ -0,0 +1,76 @@
+package resp
+
+import (
+	"net/http"
+	"reflect"
+
+	"github.com/labstack/echo/v4"
+)
+
+type StatusCode int
+
+const (
+	ok StatusCode = http.StatusOK
+)
+
+type listResponse struct {
+	Code StatusCode `json:"code"`
+	Msg  string     `json:"msg"`
+	Data datas      `json:"data"`
+}
+
+type datas struct {
+	Total int64       `json:"total,omitempty"`
+	Data  interface{} `json:"data"`
+}
+
+type dataResponse struct {
+	Code StatusCode  `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+func Success(c echo.Context) error {
+	var rjson struct {
+		Code StatusCode `json:"code"`
+		Msg  string     `json:"msg"`
+	}
+
+	rjson.Code = ok
+	rjson.Msg = "success"
+
+	return c.JSON(http.StatusOK, rjson)
+}
+
+func ListResponse(arr interface{}, total int64, c echo.Context) error {
+	if arr == nil {
+		arr = make([]interface{}, 0)
+	} else if reflect.ValueOf(arr).IsNil() {
+		arr = make([]interface{}, 0)
+	}
+	r := listResponse{
+		Data: datas{
+			Data:  arr,
+			Total: total,
+		},
+		Code: ok,
+	}
+	return c.JSON(http.StatusOK, r)
+}
+
+func Response(data interface{}, c echo.Context) error {
+	r := dataResponse{
+		Data: data,
+		Code: ok,
+	}
+
+	return c.JSON(http.StatusOK, r)
+}
+
+func StatusResponse(status int, data interface{}, code StatusCode, c echo.Context) error {
+	r := dataResponse{
+		Data: data,
+		Code: code,
+	}
+	return c.JSON(status, r)
+}
